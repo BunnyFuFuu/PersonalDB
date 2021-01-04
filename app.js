d388228c98140f03dc2c114e09d9dbed41bca1fd
Andrew Liang <aliang@ucsd.edu>
2021-01-04 00:44:49 -0800

    Added MongoDB connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./src/server/index');
+const server = new indexRouter();
 
 var app = express();
 
@@ -14,11 +15,10 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', server.router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,58 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs   = require('fs');
+const path = require('path');
+const body = require('body-parser');
+const getAllRoutes = require('../../utils/getAllRoutes');
+const { urlencoded } = require('express');
+const { MongoClient } = require("mongodb");                                                                                                                                   
+const dbConfig = require('../../db-config.json');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/**
+ * TODO: Actually define this.db
+ */
+class Server {
+    constructor() {
+        this.client = new MongoClient(dbConfig.uri);
+        this.client.connect().then(() =>{
+            console.debug(`Connected to MongoDB`);
+            this.db = this.client.db(dbConfig.db);
+        });
+        
+        
+        this.router = express.Router()
+            .use(async (req, res, next) => {
+                const token = req.get("Authorization");
+                if(token) {
+                    // Parse token, ignore until we use admin enabled endpoints
+                }
+                return next();
+            });
+        this.registerAllEndpoints();
+    }
+    registerAllEndpoints() {
+        const routes = getAllRoutes(path.join(__dirname, 'routes')).flat();
+        routes.map(route => {
+            /** @type { Endpoint } */
+            const endpoint = require(route);
+            if(!endpoint.path || !endpoint.method || !endpoint.handler) {
+                console.debug(`Incomplete endpoint at ${route}`);
+                return false;
+            }
+            if (endpoint.admin) {
+                
+                this.router.use(endpoint.path, (req, res, next)=> {
+                    // TODO: check Auth0 management API to verify user has mgt privs
+                    // TODO: also check if the user is authenticated at all
+                    next();
+                })
+            }
+            this.router.use(endpoint.path, body.json(), urlencoded({extended: true}));
+            this.router[endpoint.method](endpoint.path, endpoint.handler.bind(this));
+            
+            console.debug(`${endpoint.method} method at ${endpoint.path}`);
+        })
 
-module.exports = router;
+    }
+}
+
+module.exports = Server;
