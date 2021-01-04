52195fa687957ff7139567dce061187239714f8c
Andrew Liang <aliang@ucsd.edu>
2021-01-04 02:05:43 -0800

    Removed log message in update endpoints

--- src/server/routes/experience/update.js
+++ src/server/routes/experience/update.js
@@ -9,7 +9,6 @@ module.exports = {
         req.body._id = new ObjectID(req.body._id);
         const filter = {_id: req.body._id};
         await exp.replaceOne(filter, req.body);
-        console.log("replaced");
         res.sendStatus(200);
     }
 }
